1c21aaed383ba6d2e48f11d9cd3e8eabc45235f4
tkolaric <toni.kolaric@minus5.hr>
2025-05-05 15:42:16 +0200

    Add email notification when purchasing products

--- src/server/api/order/index.post.ts
+++ src/server/api/order/index.post.ts
@@ -1,3 +1,4 @@
+import nodemailer from 'nodemailer'
 import { defineEventHandler, readBody } from 'h3'
 import { prisma } from '~/server/utils/prisma'
 
@@ -38,7 +39,7 @@ export default defineEventHandler(async event => {
 
     const order = await prisma.order.create({
       data: {
-        quantity: body.quantity,
+        quantity: String(body.quantity),
         totalPrice: totalPrice,
         buyerFullname: body.buyerFullname,
         buyerEmail: body.buyerEmail,
@@ -55,6 +56,52 @@ export default defineEventHandler(async event => {
         }
       }
     })
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.APP_EMAIL,
+        pass: process.env.APP_EMAIL_PASSWORD
+      }
+    })
+
+    const mailOptions = {
+      from: process.env.APP_EMAIL,
+      to: [body.buyerEmail, product.contactEmail, process.env.APP_EMAIL],
+      subject: `Gastabajter.de narudžba ${order.id}`,
+      html: `
+               <a href="${process.env.APP_BASE_URL}" style="text-decoration: none"><h1 style="background-color: #409EFF; color: white; width: fit-content; padding: 0 15px 0 14px; border-radius: 4px">Gastabajter.de</h1></a>
+               <h3>Narudžba ${order.id} je zaprimljena u ${formatToDatetime(String(order.createdAt))}.</h3>
+               <br/>
+               <b><p>Kupac</p></b>
+               <p>Ime i prezime: ${order.buyerFullname}</p>
+               <p>Email: ${order.buyerEmail}</p>
+               <p>Mobitel: ${order.buyerNumber || '-'}</p>
+               <p>Adresa: ${order.buyerAddress}</p>
+               <p>Mjesto: ${order.buyerPlace}</p>
+               <p>Država: ${order.buyerCountry}</p>
+               <p>Poštanski broj: ${order.buyerZipCode}</p>
+               <p>Napomena: ${order.description || '-'}</p>
+               <br/>
+               <b><p>Prodavač</p></b>
+               <p>Email: ${product.contactEmail}</p>
+               <p>Mobitel: ${product.contactNumber}</p>
+               <p>Mjesto: ${product.locationPlace}</p>
+               <p>Država: ${product.locationCountry}</p>
+               <br/>
+               <b><p>Proizvod</p></b>
+               <p>ID: ${product.id}</p>
+               <p>Naziv: ${product.name}</p>
+               <p>Cijena jedinice: ${product.price} €</p>
+               <p>Količina: ${order.quantity}</p>
+               <p>Ukupna cijena: ${totalPrice} €</p>
+               <br/>
+               <a href="${process.env.APP_BASE_URL}/webshop/${product.id}/narudzba/${order.id}">Narudžba dostupna ovdje</a>`
+    }
+    await transporter.sendMail(mailOptions)
 
     return order
   }
